2e02c6705dd4570c9826779f7affe0e0c59cc2c9
ShashankSumanth <shashanksumanth@gmail.com>
2026-02-23 09:46:54 +0530

    Added code to read the grammar

--- parser.c
+++ parser.c
@@ -1,9 +1,7 @@
+#include "parser.h"
+
+
 struct FirstAndFollow
-{
-
-};
-
-struct Grammar
 {
 
 };
@@ -18,15 +16,72 @@ struct ParseTree
 
 };
 
+uint16_t get_symbol_id(char *symbol) {
+    for (uint16_t i = 0; i < symbol_count; i++)
+    {
+        if (strcmp(symbol_table[i], symbol) == 0)
+            return i;
+    }
 
-struct FirstAndFollow ComputeFirstAndFollowSets (struct Grammar G) {
-    /*
-        This function needs to take as input a Grammar and compute the FIRST and FOLLOW for each non terminal in the Grammar.
-        Challenges:
-            - In what format would you represent the Grammar and the First and Follow sets
-            - The method used for computing the sets is also something that needs to be solved
-            - Outside the driver you will need a function to load the Grammar when you first run it.
-    */
+    strcpy(symbol_table[symbol_count], symbol);
+    symbol_count++;
+    return symbol_count - 1;
+}
+
+void readGrammar(const char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        printf("Error opening grammar file\n");
+        exit(1);
+    }
+
+    char line[256];
+
+    while (fgets(line, sizeof(line), fp)) {
+
+        line[strcspn(line, "\n")] = '\0';
+
+        if (strlen(line) == 0 || line[0] == '#')
+            continue;
+
+        Grammar g;
+        g.rhs_length = 0;
+
+        char *token = strtok(line, " ");
+        
+        if (token[0] == '<') {
+            char clean[MAX_SYMBOL_LENGTH];
+            strcpy(clean, token[0]);
+            g.lhs = get_symbol_id(clean);
+
+        } else {
+            printf("Invalid LHS format\n");
+            exit(1);
+
+        }
+
+        token = strtok(NULL, " ");
+        if (!token || strcmp(token, "===>") != 0) {
+            printf("Expected ===>\n");
+            exit(1);
+        }
+
+        while ((token = strtok(NULL, " ")) != NULL) {
+            if (strcmp(token, "epsilon") == 0) {
+                g.rhs[g.rhs_length++] = EPSILON;
+            }
+            else {
+                // Terminal (ALL CAPS)
+                g.rhs[g.rhs_length++] = get_symbol_id(token);
+            }
+        }
+        grammar[grammar_count++] = g;
+    }
+    fclose(fp);
+}
+
+struct FirstAndFollow ComputeFirstAndFollowSets (const char *filename) {
+   readGrammar(filename);
 }
 
 void createParseTable(struct FirstAndFollow F, struct Table T) {
--- parser.h
+++ parser.h
@@ -1,1 +1,22 @@
 // Contains function prototype declarations of functions in parser.c
+#include <stdint.h>
+
+#define MAX_RHS_LENGTH 32
+#define MAX_SYMBOL_LENGTH 40
+#define MAX_GRAMMAR_LENGTH 64
+#define MAX_SYMBOLS 128
+#define EPSILON 65535
+
+char symbol_table[MAX_SYMBOLS][MAX_SYMBOL_LENGTH];
+uint16_t symbol_count = 0;
+
+Grammar grammar[MAX_GRAMMAR_LENGTH];
+uint16_t grammar_count = 0;
+
+typedef struct
+{
+    uint16_t lhs;
+    uint16_t rhs[MAX_RHS_LENGTH];
+    uint16_t rhs_length;
+
+} Grammar;
